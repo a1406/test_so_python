0a3be23486a0d95f247171fd83a2148e3af29a3b
jack at ycat <jack@company>
2016-08-20 10:20:36 +0800

    1

---
+++ comm_head.h
@@ -0,0 +1,6 @@
+#ifndef __COMM_HEAD_H
+#define  __COMM_HEAD_H
+
+
+
+#endif /*   */
--- main.cpp
+++ main.cpp
@@ -2,10 +2,53 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <dlfcn.h>
+#include "comm_head.h"
+
+class player
+{
+public:
+	char *get_name()
+	{
+		return m_name;
+	}
+	int get_age()
+	{
+		return m_age;
+	}
+	int get_id()
+	{
+		return m_id;
+	}
+	void set_name(char *name)
+	{
+		m_name = strdup(name);
+	}
+	void set_age(int age)
+	{
+		m_age = age;
+	}
+	void set_id(int id)
+	{
+		m_id = id;
+	}
+
+	int m_id;
+	char *m_name;
+	int m_age;
+};
+
+static player all_players[100];
 
 int main(int argc, char *argv[])
 {
-    
+    printf("%s %d\n", __FUNCTION__, __LINE__);
+	for (int i = 0; i < 100; ++i)
+	{
+		all_players[i].m_id = i;
+		all_players[i].m_age = 10;
+		all_players[i].m_name = (char *)"no name";				
+	}
     return 0;
 }
 
